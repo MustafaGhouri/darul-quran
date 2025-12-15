ee2c92e14b22c067514a7219bb724b9c2a6b3238
mazz <112586426+MustafaGhouri@users.noreply.github.com>
2025-12-15 10:46:29 +0500

    pushaaaa

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
         theme_color: "#0d6efd",
         icons: [
           {
-            src: "/icons/logo.png",
+            src: "/icons/darul-quran-logo.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/logo.png",
+            src: "/icons/darul-quran-logo.png",
             sizes: "512x512",
             type: "image/png",
           },
